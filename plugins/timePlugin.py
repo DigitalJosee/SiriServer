79ff3530b66f7be9348e42521f0183d722dc4c26
DigitalJosee <jdmuitobom@gmail.com>
2012-02-09 12:58:38 -0200

    Portuguese Support for time plugin

--- plugins/timePlugin.py
+++ plugins/timePlugin.py
@@ -7,23 +7,55 @@ import json
 
 from plugin import *
 
+from siriObjects.baseObjects import AceObject, ClientBoundCommand
 from siriObjects.uiObjects import AddViews, AssistantUtteranceView
-from siriObjects.clockObjects import ClockSnippet, ClockObject
+from siriObjects.systemObjects import DomainObject
+
+class ClockSnippet(AceObject):
+    def __init__(self, clocks=None):
+        super(ClockSnippet, self).__init__("Snippet", "com.apple.ace.clock")
+        self.clocks = clocks if clocks != None else []
+
+    def to_plist(self):
+        self.add_property('clocks')
+        return super(ClockSnippet, self).to_plist()
+
+class ClockObject(DomainObject):
+    def __init__(self):
+        super(ClockObject, self).__init__("com.apple.ace.clock")
+        self.unlocalizedCountryName = None
+        self.unlocalizedCityName = None
+        self.timezoneId = None
+        self.countryName = None
+        self.countryCode = None
+        self.cityName = None
+        self.alCityId = None
+    
+    def to_plist(self):
+        self.add_property('unlocalizedCountryName')
+        self.add_property('unlocalizedCityName')
+        self.add_property('timezoneId') 
+        self.add_property('countryName')
+        self.add_property('countryCode')
+        self.add_property('cityName')
+        self.add_property('alCityId')
+        return super(ClockObject, self).to_plist()
+
 
 ####### geonames.org API username ######
 geonames_user="test2"
 
-class timePlugin(Plugin):
+class time(Plugin):
     
     localizations = {"currentTime": 
-                        {"search":{"de-DE": "Es wird gesucht ...", "en-US": "Looking up ..."}, 
-                         "currentTime": {"de-DE": "Es ist @{fn#currentTime}", "en-US": "It is @{fn#currentTime}"}}, 
+                        {"search":{"de-DE": "Es wird gesucht ...", "en-US": "Procurando ..."}, 
+                         "currentTime": {"de-DE": "Es ist @{fn#currentTime}", "en-US": "Sao @{fn#currentTime}"}}, 
                      "currentTimeIn": 
-                        {"search":{"de-DE": "Es wird gesucht ...", "en-US": "Looking up ..."}, 
+                        {"search":{"de-DE": "Es wird gesucht ...", "en-US": "Procurando ..."}, 
                          "currentTimeIn": 
                                 {
-                                "tts": {"de-DE": u"Die Uhrzeit in {0},{1} ist @{{fn#currentTimeIn#{2}}}:", "en-US": "The time in {0},{1} is @{{fn#currentTimeIn#{2}}}:"},
-                                "text": {"de-DE": u"Die Uhrzeit in {0}, {1} ist @{{fn#currentTimeIn#{2}}}:", "en-US": "The time in {0}, {1} is @{{fn#currentTimeIn#{2}}}:"}
+                                "tts": {"de-DE": u"Die Uhrzeit in {0},{1} ist @{{fn#currentTimeIn#{2}}}:", "en-US": "The time in {0}, {1} is @{{fn#currentTimeIn#{2}}}:"},
+                                "text": {"de-DE": u"Die Uhrzeit in {0}, {1} ist @{{fn#currentTimeIn#{2}}}:", "en-US": "As horas em {0}, {1} sao @{{fn#currentTimeIn#{2}}}:"}
                                 }
                         },
                     "failure": {
@@ -32,16 +64,16 @@ class timePlugin(Plugin):
                     }
 
     @register("de-DE", "(Wie ?viel Uhr.*)|(.*Uhrzeit.*)")     
-    @register("en-US", "(What.*time.*)|(.*current time.*)")
+    @register("en-US", "(Que horas sao*)|(.*Horas.*)|(.*Hora.*)")
     def currentTime(self, speech, language):
         #first tell that we look it up
         view = AddViews(self.refId, dialogPhase="Reflection")
-        view.views = [AssistantUtteranceView(text=timePlugin.localizations['currentTime']['search'][language], speakableText=timePlugin.localizations['currentTime']['search'][language], dialogIdentifier="Clock#getTime")]
+        view.views = [AssistantUtteranceView(text=time.localizations['currentTime']['search'][language], speakableText=time.localizations['currentTime']['search'][language], dialogIdentifier="Clock#getTime")]
         self.sendRequestWithoutAnswer(view)
         
         # tell him to show the current time
         view = AddViews(self.refId, dialogPhase="Summary")
-        view1 = AssistantUtteranceView(text=timePlugin.localizations['currentTime']['currentTime'][language], speakableText=timePlugin.localizations['currentTime']['currentTime'][language], dialogIdentifier="Clock#showTimeInCurrentLocation")
+        view1 = AssistantUtteranceView(text=time.localizations['currentTime']['currentTime'][language], speakableText=time.localizations['currentTime']['currentTime'][language], dialogIdentifier="Clock#showTimeInCurrentLocation")
         clock = ClockObject()
         clock.timezoneId = self.connection.assistant.timeZoneId
         view2 = ClockSnippet(clocks=[clock])
@@ -50,10 +82,10 @@ class timePlugin(Plugin):
         self.complete_request()
     
     @register("de-DE", "(Wieviel Uhr.*in ([\w ]+))|(Uhrzeit.*in ([\w ]+))")
-    @register("en-US", "(What.*time.*in ([\w ]+))|(.*current time.*in ([\w ]+))")
+    @register("en-US", "(Que horas sao.*em ([\w ]+))|(.*Horas.*em ([\w ]+))")
     def currentTimeIn(self, speech, language):
         view = AddViews(self.refId, dialogPhase="Reflection")
-        view.views = [AssistantUtteranceView(text=timePlugin.localizations['currentTimeIn']['search'][language], speakableText=timePlugin.localizations['currentTimeIn']['search'][language], dialogIdentifier="Clock#getTime")]
+        view.views = [AssistantUtteranceView(text=time.localizations['currentTimeIn']['search'][language], speakableText=time.localizations['currentTimeIn']['search'][language], dialogIdentifier="Clock#getTime")]
         self.sendRequestWithoutAnswer(view)
         
         error = False
@@ -108,7 +140,7 @@ class timePlugin(Plugin):
                             countryCode = filter(lambda x: True if "country" in x['types'] else False, components)[0]['short_name']
                             
                             view = AddViews(self.refId, dialogPhase="Summary")
-                            view1 = AssistantUtteranceView(text=timePlugin.localizations['currentTimeIn']['currentTimeIn']['text'][language].format(city, country, timeZone), speakableText=timePlugin.localizations['currentTimeIn']['currentTimeIn']['tts'][language].format(city, country, timeZone), dialogIdentifier="Clock#showTimeInOtherLocation")
+                            view1 = AssistantUtteranceView(text=time.localizations['currentTimeIn']['currentTimeIn']['text'][language].format(city, country, timeZone), speakableText=time.localizations['currentTimeIn']['currentTimeIn']['tts'][language].format(city, country, timeZone), dialogIdentifier="Clock#showTimeInOtherLocation")
                             clock = ClockObject()
                             clock.timezoneId = timeZone
                             clock.countryCode = countryCode
@@ -131,7 +163,7 @@ class timePlugin(Plugin):
             error = True
         if error:
             view = AddViews(self.refId, dialogPhase="Completion")
-            view.views = [AssistantUtteranceView(text=timePlugin.localizations['failure'][language], speakableText=timePlugin.localizations['failure'][language], dialogIdentifier="Clock#cannotShowClocks")]
+            view.views = [AssistantUtteranceView(text=time.localizations['failure'][language], speakableText=time.localizations['failure'][language], dialogIdentifier="Clock#cannotShowClocks")]
             self.sendRequestWithoutAnswer(view)
         self.complete_request()
 
